1a8a0668467d821f90c90992ee1eeedb3c08fe16
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2025-07-28 11:17:03 +0700

    change: middleware and version of clerkjs

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -38,8 +38,9 @@ const CourseCreationPage = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-        const response = await axios.post("/api/course", values)
-        router.push(`teacher/courses/${response.data.id}`)
+        const response = await axios.post("/api/courses", values)
+        router.push(`/teacher/courses/${response.data.id}`)
+        toast.success("Course Created");
     } catch {
         toast("Something went wrong");
     }
